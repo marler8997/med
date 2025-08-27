192f036e271a46e28d70393484032232e30ee994
Jonathan Marler <johnnymarler@gmail.com>
2025-08-27 06:18:10 -0600

    move syntax highlighting code into subdirectory

---
+++ src/highlight.zig
@@ -0,0 +1,26 @@
+pub const zig = @import("highlight/zig.zig");
+
+pub const TokenKind = enum {
+    todo,
+    unknown,
+    keyword,
+    string_literal,
+    operator,
+    doc_comment,
+    comment,
+    pub fn color(self: TokenKind) zin.Rgb8 {
+        return switch (self) {
+            // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            .todo => theme.fg,
+            .unknown => .{ .r = 0xff, .g = 0x33, .b = 0x33 },
+            .keyword => .{ .r = 0xf7, .g = 0xa4, .b = 0x1d },
+            .string_literal => .{ .r = 0x3c, .g = 0x51, .b = 0x90 },
+            .operator => .{ .r = 0x04, .g = 0x96, .b = 0xff },
+            .doc_comment => .{ .r = 0x20, .g = 0x83, .b = 0x73 },
+            .comment => .{ .r = 0x3a, .g = 0xcf, .b = 0xc8 },
+        };
+    }
+};
+
+const zin = @import("zin");
+const theme = @import("theme.zig");
